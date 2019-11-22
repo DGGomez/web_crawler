1b0de9d00557a84b94e0471f4844eb61f9e23f01
Daniel Gomez <dggomez21@gmail.com>
2019-11-22 18:42:10 -0500

    update

--- main.py
+++ main.py
@@ -1,49 +1,91 @@
 from html.parser import HTMLParser  
-from urllib.request import urlopen  
+from urllib.request import urlopen as uReq
 from urllib import parse
-
+import bs4
+from bs4 import BeautifulSoup as soup
+import math
+import re
+import sys
 import time
+
+def contains_digits(s):
+    return any(char.isdigit() for char in s)
+
+def tf(f):
+    return 1 + math.log(f)
+
+def w(tf, idf):
+    return tf * idf
+
+def sim(wi,wq):
+
+    di = 0
+    dNi = 0
+    dNq = 0
+    size = 0
+    if len(wi) > len(wq):
+        size = len(wq)
+    else:
+        size = len(wi)
+    for i in range(size):
+        di += wi[i] * wq[i]
+        dNi += wi[i] * wi[i]
+        dNq += wq[i] * wq[i]
+    dNi = math.sqrt(dNi)
+    dNq = math.sqrt(dNq)
+    return ((di)/(dNi*dNq))
+
 start = time.process_time()
 
-class MyHTMLParser(HTMLParser):
-    def handle_starttag(self, tag, attrs):
-        print("Encountered a start tag:"+ tag)
-
-    def handle_endtag(self, tag):
-        print("Encountered an end tag :"+ tag)
-
-    def handle_data(self, data):
-        print("Encountered some data  :"+ data)
-        
 K = 10
 pages = {}
 content = []
 links = []
+wordList = []
+numberList = []
 # define topic: Workout
-
-# search for workout information
 name = 'bodybuilding'
-url = 'https://www.bodybuilding.com/category/workouts'
 
 # get initial web pages
-response = urlopen(url)
-htmlBytes = response.read()
-htmlString = htmlBytes.decode("utf-8")
-
 try:
-
+    myUrl = 'https://www.bodybuilding.com/category/workouts'
+    uClient = uReq(myUrl)
+    page_html = uClient.read()
+    uClient.close()
     print(" **Success!**")
+
+    # generate dictionary
+    page_soup = soup(page_html, "html.parser")
+    links = page_soup.findAll("a", href=True)
+
+    for i in page_soup:
+        for word in i:
+            if word in wordList:
+                index = wordList.index(word)
+                numberList[index] += 1
+            else:
+                wordList.append(word)
+                numberList.append(1)
+    
+    # go through links and check similarity
+    for a in links:
+        # check robot.txt
+
+        # read data
+        uClient = uReq(a)
+        page_html = uClient.read()
+        uClient.close()
+
+        # get values
+        page_soup = soup(page_html, "html.parser")
+        links = page_soup.findAll("a", href=True)
+
+        # check similarity (pick threshold value)
+        # stop crawling
+
+
 except:
     print(" **Failed!**")
-
-parser = MyHTMLParser()
-parser.feed(response)
-
-# check similarity (pick threshold value)
-
-# check robot.txt
-
-# stop crawling
 
 # write results
 fileOut = open("results.txt", "w+")
