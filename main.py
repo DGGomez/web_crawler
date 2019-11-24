e47be46d7ab5e6aabff278a67212f6e1857b9f25
Daniel Gomez <dggomez21@gmail.com>
2019-11-24 14:52:09 -0500

    update

--- main.py
+++ main.py
@@ -46,55 +46,93 @@ links = []
 wordList = []
 numberList = []
 visited = []
-# define topic: Workout
-name = 'bodybuilding'
+postings = {}
+passList = []
+frequencyList = {}
 
+# define topic: Workout
+name = 'workout'
+start_url = ["https://www.t-nation.com/","https://www.bodybuilding.com/category/workouts","https://www.muscleandstrength.com/workout-routines"]
+robot_url = ['https://www.t-nation.com/robots.txt', 'https://www.bodybuilding.com/robots.txt', 'https://www.muscleandstrength.com/workout-routines']
 # get initial web pages
 try:
-    myUrl = 'https://www.bodybuilding.com/category/workouts'
-    uClient = uReq(myUrl)
-    page_html = uClient.read()
-    uClient.close()
-    visited.append(myUrl)
-    # get robot list for initial website
-    myUrl2 = 'https://www.bodybuilding.com/robots.txt'
-    uClient2 = uReq(myUrl2)
-    page_html2 = uClient2.read()
-    uClient2.close()
-    p = page_html2.splitlines()
-    disallowed = []
-    for i in p:
-        val = str(i)
-        if "Disallow" in val:
-            vals = val.split(" ")
-            if val[1] == " ":
-                continue
-            disallowed.append(vals[1][:-1])
+    for num in range(len(start_url)):
+        myUrl = start_url[num]
+        uClient = uReq(myUrl)
+        page_html = uClient.read()
+        uClient.close()
+        visited.append(myUrl)
+        # get robot list for initial website
+        myUrl2 = robot_url[num]
+        uClient2 = uReq(myUrl2)
+        page_html2 = uClient2.read()
+        uClient2.close()
+        p = page_html2.splitlines()
+        disallowed = []
 
-    robot[myUrl2] = disallowed
+        for i in p:
+            val = str(i)
+            if "Disallow" in val:
+                vals = val.split(" ")
+                if val[1] == " ":
+                    continue
+                disallowed.append(vals[1][:-1])
 
-    # generate dictionary
-    page_soup = soup(page_html, "html.parser")
-    links = page_soup.findAll("a", href=True)
+        robot[myUrl2] = disallowed
 
-    for i in page_soup.find_all('div'):
-        words = i.get_text().split(" ")
-        for word in words:
+        # generate dictionary
+        page_soup = soup(page_html, "html.parser")
+        for i in page_soup.findAll("a", href=True):
+            links.append(i)
 
-            # filter word
-            if word == '':
-                continue
-            word = re.sub(r'\W+', '', word.rstrip()).lower()
+        for i in page_soup.find_all('div'):
+            words = i.get_text().split(" ")
+            for word in words:
 
-            # store in wordList
-            if word in wordList:
-                index = wordList.index(word)
-                numberList[index] += 1
-            else:
-                wordList.append(word)
-                numberList.append(1)       
+                # filter word
+                if word == '':
+                    continue
+                word = re.sub(r'\W+', '', word.rstrip()).lower()
+
+                # store in wordList
+                if word in wordList:
+                    index = wordList.index(word)
+                    numberList[index] += 1
+                    postings[word] += 1
+                else:
+                    wordList.append(word)
+                    numberList.append(1)
+                    postings[word] = 1
+
+    # sim logic
+
+    # remove low idf values
+    # for line in postings:
+    #     idf = math.log(3/int(postings[line]))
+    #     if idf >= 0.3:
+    #         passList.append(line)
+    #     val = postings[line]
+    #     frequencyList[line] = [val,idf]
+
+    # wq = []
+    # wadjusted = []
+    # # get query weights
+    # for i in range(len(numberList)):
+    #     if numberList[i] > 0:            
+    #         # fetch frequency
+    #         tfi = tf(numberList[i])
+    #         idf = frequencyList.get(wordList[i])
+    #         if(idf == None):
+    #             continue
+    #         wq.append(w(tfi, idf[1]))
+    #     else:
+    #         wq.append(0)
+
     # go through links and check similarity
+
     for a in links:
+        # time delay 1 second
+        time.sleep(1)
         # check robot.txt
         if "https://" in a:
             url = a[8:].split("/")
@@ -133,13 +171,19 @@ try:
 
         # get values
         page_soup = soup(page_html, "html.parser")
+        
+        # add pages links to link to links array??
         # links = page_soup.findAll("a", href=True)
 
-        # check similarity (pick threshold value) and extract info
+        # extract info
+        pageNumberList = [0]* len(wordList)
+        content = []
         for i in page_soup.find_all('div'):
+            # get page info and store
+            content.append(i.get_text())
             words = i.get_text().split(" ")
-            for word in words:
 
+            for word in words:
                 # filter word
                 if word == '':
                     continue
@@ -148,14 +192,27 @@ try:
                 # store in wordList
                 if word in wordList:
                     index = wordList.index(word)
-                    numberList[index] += 1
-                else:
-                    wordList.append(word)
-                    numberList.append(1)     
+                    pageNumberList[index] += 1
+
+        # check similarity
+        wi = []
+        threshold = 0.75
+        checkVals = 0
+        numberVal = len(numberList)
+        # get document list and check k-rating
+        for j in range(len(pageNumberList[i])):
+             # contains 90% of query values
+            if int(pageNumberList[j]) > 0 and int(numberList[j]) > 0:
+                checkVals+=1
+
+        if (checkVals/numberVal) >threshold:
+            # add to list
+            pages[a] = content
 
         # stop crawling
         if len(pages) >= K:
             break
+
 except Exception:
     traceback.print_exc()
 
